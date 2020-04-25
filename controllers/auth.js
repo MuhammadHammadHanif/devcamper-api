7231273cfc15e71a466e6df06ac49bca064ddc21
Muhammad Hammad <hammadhanif0@gmail.com>
2020-04-26 01:18:40 +0500

    Create JWT Token

--- controllers/auth.js
+++ controllers/auth.js
@@ -8,8 +8,18 @@ const User = require('../models/User');
 // @route   GET /api/v1/auth/register
 // @access  Public
 exports.register = asyncHandler(async (req, res, next) => {
-  // Create User
-  await User.create(req.body);
+  const { name, email, password, role } = req.body;
 
-  res.status(201).json({ success: true });
+  // Create User
+  const user = await User.create({
+    name,
+    email,
+    password,
+    role,
+  });
+
+  // Create token
+  const token = user.getSignedJwtToken();
+
+  res.status(201).json({ success: true, token });
 });
