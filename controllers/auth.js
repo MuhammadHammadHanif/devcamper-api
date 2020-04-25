b8d7ef3320984e79501e8d8bd2a67bac87fa994e
Muhammad Hammad <hammadhanif0@gmail.com>
2020-04-25 18:24:46 +0500

    Create User model and setup controller and route files

---
+++ controllers/auth.js
@@ -0,0 +1,12 @@
+const ErrorResponse = require('../utils/errorResponse');
+const asyncHandler = require('../middleware/async');
+
+// Model
+const User = require('../models/User');
+
+// @desc    Register User
+// @route   GET /api/v1/auth/register
+// @access  Public
+exports.register = asyncHandler(async (req, res, next) => {
+  res.status(201).json({ succes: true });
+});
