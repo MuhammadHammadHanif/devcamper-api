c8961a74d1c492c1fa12170bc581dbc3eb86967e
Muhammad Hammad <hammadhanif0@gmail.com>
2020-04-26 02:48:48 +0500

    Create Get current logged in user

--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,14 @@
 const express = require('express');
 
-const { register, login } = require('../controllers/auth');
+const { register, login, getMe } = require('../controllers/auth');
+
+// Middleware
+const { protect } = require('../middleware/auth');
 
 const router = express.Router();
 
 router.route('/register').post(register);
-
 router.route('/login').post(login);
+router.route('/me').get(protect, getMe);
 
 module.exports = router;
