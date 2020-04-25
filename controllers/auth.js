207c2731e3982c8ba9f6c13c956f1db6eb16f346
Muhammad Hammad <hammadhanif0@gmail.com>
2020-04-26 01:01:39 +0500

    Create Middleware for hash password in User Model

--- controllers/auth.js
+++ controllers/auth.js
@@ -8,5 +8,8 @@ const User = require('../models/User');
 // @route   GET /api/v1/auth/register
 // @access  Public
 exports.register = asyncHandler(async (req, res, next) => {
-  res.status(201).json({ succes: true });
+  // Create User
+  await User.create(req.body);
+
+  res.status(201).json({ success: true });
 });
