16f42ffcfd82ceee0ff01e17b3d41fe87689461e
Muhammad Hammad <hammadhanif0@gmail.com>
2020-04-27 03:28:03 +0500

    Add XSS attack protection and security header

--- server.js
+++ server.js
@@ -3,12 +3,16 @@ const express = require('express');
 const dotenv = require('dotenv');
 const fileupload = require('express-fileupload');
 const cookieParser = require('cookie-parser');
+const morgan = require('morgan');
+
+// Security
 const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
+const xss = require('xss-clean');
 
 const connectDB = require('./config/db');
 
-// Middleware
-const morgan = require('morgan');
+// Custom Middleware
 const errorHandler = require('./middleware/error');
 
 // Load ENV vars
@@ -42,6 +46,12 @@ app.use(fileupload());
 
 // Sanitize data
 app.use(mongoSanitize());
+
+// Set security headers
+app.use(helmet());
+
+// Prevent XSS attack
+app.use(xss());
 
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
