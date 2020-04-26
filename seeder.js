ed76ddcbc5339eea18883c75e6c5a5bb32245f9b
Muhammad Hammad <hammadhanif0@gmail.com>
2020-04-26 05:04:59 +0500

    Create User RelationShip

--- seeder.js
+++ seeder.js
@@ -8,6 +8,7 @@ dotenv.config({ path: './config/config.env' });
 // Load Models
 const Bootcamp = require('./models/Bootcamp');
 const Course = require('./models/course');
+const User = require('./models/User');
 
 // Connect to DB
 mongoose.connect(process.env.MONGO_URI, {
@@ -26,11 +27,16 @@ const courses = JSON.parse(
   fs.readFileSync(`${__dirname}/_data/courses.json`, 'utf-8')
 );
 
+const users = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/users.json`, 'utf-8')
+);
+
 // Import into DB
 const importData = async () => {
   try {
     await Bootcamp.create(bootcamps);
     await Course.create(courses);
+    await Course.create(users);
     console.log('Data Imported...');
     process.exit();
   } catch (err) {
@@ -43,6 +49,7 @@ const deleteData = async () => {
   try {
     await Bootcamp.deleteMany();
     await Course.deleteMany();
+    await User.deleteMany();
 
     console.log('Data Destroyed...');
     process.exit();
