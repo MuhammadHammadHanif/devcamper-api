df49ff2386f5ab6408883c5c894b4231f2c0b271
Muhammad Hammad <hammadhanif0@gmail.com>
2020-04-27 03:38:23 +0500

    Add API Rate Limit and Prevent http params pollution

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ const morgan = require('morgan');
 const mongoSanitize = require('express-mongo-sanitize');
 const helmet = require('helmet');
 const xss = require('xss-clean');
+const rateLimit = require('express-rate-limit');
+const hpp = require('hpp');
 
 const connectDB = require('./config/db');
 
@@ -52,6 +54,16 @@ app.use(helmet());
 
 // Prevent XSS attack
 app.use(xss());
+
+// Rate limiting
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000, // 10 min
+  max: 100,
+});
+app.use(limiter);
+
+// Prevent http param pollution
+app.use(hpp());
 
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
